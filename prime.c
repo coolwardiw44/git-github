4dd035ad10ac89e3ab308b142539fabb16b40bab
coolwardiw44 <coolwardiw44@gmail.com>
2019-07-14 17:04:52 +0530

    pgm for prime

---
+++ prime.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int num;
+	int i, flag;
+	scanf("%d",&num);
+	for ( i=2; i<= sqrt(num); i++)
+	{
+		if (num%2==0)
+		{
+			flag=1;
+			printf("number is not prime:\n");
+		}
+		else {
+			printf("number is prime\n");
+		}
+	}
+
+	return 0;
+}
